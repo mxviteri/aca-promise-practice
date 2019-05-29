8a7d322fa0508e0af0855b9f6b9bf5add8f9a9bf
Michael Snowden <mtsnowden@gmail.com>
2019-05-28 20:23:31 -0500

    started. confused af

--- src/Users.js
+++ src/Users.js
@@ -1,13 +1,21 @@
-import React, { Component, Fragment } from 'react'
-import { Button } from '@material-ui/core'
+import React, { Component, Fragment } from 'react';
+import { Button } from '@material-ui/core';
+import { connect } from 'react-redux';
+
 
 class Users extends Component {
+state = {
+  users: []
+}
+
+getUsers=()=> {
+}
   render() {
     return (
       <Fragment>
         <div>
           <h3>Press the button to get users and display them below</h3>
-          <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
+          <Button onClick={this.state.getUsers} variant="contained" color="primary">Get Users</Button>
         </div>
         <div className="users-block">
           {/* Map through users here */}
@@ -17,4 +25,16 @@ class Users extends Component {
   }
 }
 
-export default Users
+const mapStateToProp = (state) => {
+  return {
+    users: state.users
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUsers: (users) => dispatchEvent(setUsers(users))
+  }
+}
+
+export default connect(mapStateToProp, mapDispatchToProps)(Users);
